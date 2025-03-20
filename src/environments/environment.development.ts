7620acbbae95137cb44f6eab5e61c1cea265666b
desoga10 <done2stay@gmail.com>
2025-03-20 01:13:51 +0100

    Integrated Frankfurther api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,4 +3,5 @@ export const environment = {
   supabaseUrl: 'https://oayxnmdianrhxwcdmcpr.supabase.co',
   supabaseKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9heXhubWRpYW5yaHh3Y2RtY3ByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0NDAzMTEsImV4cCI6MjA1NjAxNjMxMX0.5VIcR_Q5ghplYboM-LLXJ2dEV6ghiZQ77cDOURu76jY',
+  API_URL: 'https://api.frankfurter.app',
 };

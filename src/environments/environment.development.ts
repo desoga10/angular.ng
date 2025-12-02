4b2b772d67fc9c5f0892ba83c89924541a2e2a9a
deji adesoga <done2stay@gmail.com>
2025-12-02 01:21:26 +0100

    Update Supabase key in development environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   supabaseUrl: 'https://duzorsdkwvvlnmmtgvxi.supabase.co  ',
   supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR1em9yc2Rrd3Z2bG5tbXRndnhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMyNTMzNTgsImV4cCI6MjA3ODgyOTM1OH0.Qx1WA3SjtxLJ7OCD9gHJWYs8UHA9BvTk-ZVnceKM6wc',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9heXhubWRpYW5yaHh3Y2RtY3ByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0NDAzMTEsImV4cCI6MjA1NjAxNjMxMX0.5VIcR_Q5ghplYboM-LLXJ2dEV6ghiZQ77cDOURu76jY',
   FRANKFURTER_API_URL: 'https://api.frankfurter.app',
   EXCHANGE_RATE_API_URL:
     'https://v6.exchangerate-api.com/v6/6eb99285a0390c91620a279c/pair',

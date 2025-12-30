002715d5eee0cab29790ff43d00973b5b713ba67
Afreen Saleem <afreen786saleem123@gmail.com>
2025-12-30 15:04:40 -0500

    updated supabaseUrl

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://duzorsdkwvvlnmmtgvxi.supabase.co  ',
+  supabaseUrl: 'https://oayxnmdianrhxwcdmcpr.supabase.co',
   supabaseKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9heXhubWRpYW5yaHh3Y2RtY3ByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0NDAzMTEsImV4cCI6MjA1NjAxNjMxMX0.5VIcR_Q5ghplYboM-LLXJ2dEV6ghiZQ77cDOURu76jY',
   FRANKFURTER_API_URL: 'https://api.frankfurter.app',
